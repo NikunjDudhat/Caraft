ee2ab55a5f077ff0cc8548f52559550cb095a207
nikunjdudhat <nikunjdudhat2002@gmail.com>
2022-09-27 09:02:14 +0530

    total amount fix

--- src/Container/CartDetails/CartDetails.js
+++ src/Container/CartDetails/CartDetails.js
@@ -111,35 +111,6 @@ function CartDetails(props) {
                 </div>
             </div>
         </div>
-
-        // <div className='product_details'>
-        //     <div className='container'>
-        //         <div className="row">
-        //             {
-        //                 ProductDetail.map((p) => (
-        //                     <>
-        //                         <div className="col-lg-6 col-sm-12">
-        //                             <div className='product_img mb-5'>
-        //                                 <img src={p.url} />
-        //                             </div>
-        //                             <div className='addcart'>
-        //                                 <button className='btn' onClick={hendleCart(p.id)}><ShoppingCartIcon /> Add to cart</button>
-        //                                 <button className='btn'><BoltIcon /> Buy now</button>
-        //                             </div>
-        //                         </div>
-        //                         <div className="col-lg-6 col-sm-12">
-        //                             <h2>{p.product_name}</h2>
-        //                             <p className='price'><span><b>Price :</b></span> ₹{p.product_price}</p>
-        //                             <div className='d-flex descrption'>
-        //                                 <p><b>Description </b></p>
-        //                                  <span className='m-0'>{p.product_description}</span></div>
-        //                         </div>
-        //                     </>
-        //                 ))
-        //             }
-        //         </div>
-        //     </div>
-        // </div>
     );
 }
 
