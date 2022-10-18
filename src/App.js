695492d213769cd05e9732d189aa5d14c3ce6f13
nikunjdudhat <nikunjdudhat2002@gmail.com>
2022-10-18 09:08:54 +0530

    fixed public and private route

--- src/App.js
+++ src/App.js
@@ -3,52 +3,29 @@ import { Route, Switch } from 'react-router-dom';
 import "./App.css"
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
-import Home from './Container/Home/Home';
-import Category from './Container/Category/Category';
-import Products from './Container/Products/Products';
-import Client from './Container/Client/Client';
-import Contact from './Container/Contact/Contact';
-import Login from './Container/Login/Login';
-import Categ_admin from './admin_panel/Container/Categ_admin';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './Redux/Store';
-import Product_admin from './admin_panel/Container/Product_admin';
-import ProductDetails from './Container/ProductDetails/ProductDetails';
-import CartDetails from './Container/CartDetails/CartDetails';
-import OrderAdmin from './admin_panel/Container/Order_admin';
 import 'react-toastify/dist/ReactToastify.css';
 import ToastContainerCompnent from './Components/ToastContainer/ToastContainerCompnent';
 import { SnackbarProvider } from 'notistack';
+import AppRoute from './Route/AppRoute';
 
 
 function App() {
 
   return (
     <>
-    <SnackbarProvider maxSnack={3}>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-      <Header/>
-      {/* <Home/> */}
-        <Switch>
-          <Route exact path={"/"} component={Home} />
-          <Route exact path={"/Category"} component={Category} />
-          <Route exact path={"/product-details"} component={ProductDetails} />
-          {/* <Route exact path={"/admin_Category"} component={Cate_admin} /> */}
-          <Route exact path={"/admin_Category"} component={Categ_admin} />
-          <Route exact path={"/product_admin"} component={Product_admin} />
-          <Route exact path={"/Products"} component={Products} />
-          <Route exact path={"/Client"} component={Client} />
-          <Route exact path={"/Contact"} component={Contact} />
-          <Route exact path={"/Login"} component={Login} />
-          <Route exact path={"/CartDetails"} component={CartDetails} />
-          <Route exact path={"/OrderDetails"} component={OrderAdmin} />
-        </Switch>
-      <Footer/>
-      </PersistGate>
-      </Provider>
-      <ToastContainerCompnent /> 
+      <SnackbarProvider maxSnack={3}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Header />
+            {/* <Home/> */}
+            <AppRoute />
+            <Footer />
+          </PersistGate>
+        </Provider>
+        <ToastContainerCompnent />
       </SnackbarProvider>
     </>
   );
